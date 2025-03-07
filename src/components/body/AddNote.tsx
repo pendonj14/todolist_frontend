b998a6060a0bd2ae5a964a8d4d111936cd73ab40
pendonj14 <pendon.josephjr@gmail.com>
2025-03-07 16:42:36 +0800

    for vercel

--- src/components/body/AddNote.tsx
+++ src/components/body/AddNote.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import api from "../../api/api";
-
+import { Note } from "@/types/types";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 interface AddNoteProps {
-    setNotes: (notes: any[]) => void;
+    setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
     showModalMessage: (message: string) => void;
 }
 
 const AddNote: React.FC<AddNoteProps> = ({ setNotes, showModalMessage }) => {
     const [content, setContent] = useState<string>("");
-
+    const {theme} = useTheme();
     
     const getNotes = async () => {
         try {
@@ -21,7 +22,6 @@ const AddNote: React.FC<AddNoteProps> = ({ setNotes, showModalMessage }) => {
         }
     };
 
-    
     const createNote = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
@@ -38,7 +38,7 @@ const AddNote: React.FC<AddNoteProps> = ({ setNotes, showModalMessage }) => {
     };
 
     return (
-        <div className="flex flex-col justify-between bg-[#B3B3B3] m-3 p-3 rounded-lg min-h-[200px] text-center motion-preset-expand motion-duration-1000">
+        <div className={`flex flex-col justify-between ${theme == 'dark' ? 'bg-[#2a2a2a]': 'bg-[#B3B3B3]'} m-3 p-3 rounded-lg min-h-[200px] text-center motion-preset-expand motion-duration-1000`}>
             <form onSubmit={createNote}>
                 <textarea
                     className="resize-none bg-inherit border-none outline-none text-center pt-[55px]"
