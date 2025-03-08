082967fc24e6884cc4ccce92a121758b807e21f6
pendonj14 <pendon.josephjr@gmail.com>
2025-03-08 18:32:34 +0800

    updated env for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://52.62.239.119:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
