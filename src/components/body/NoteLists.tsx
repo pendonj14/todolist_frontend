230a53d0a2311caabb60dd4073cad3bb981a79ab
pendonj14 <pendon.josephjr@gmail.com>
2025-03-12 08:18:49 +0800

    center mobile view

--- src/components/body/NoteLists.tsx
+++ src/components/body/NoteLists.tsx
@@ -22,7 +22,7 @@ const NoteLists: React.FC<NoteListsProps> = ({ notes, deleteNote, showModalMessa
     };
 
     return (
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 p-10 pl-25 pr-25">
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 p-10 pl-25 pr-25 justify-center mx-auto">
             {notes.map((note) => (
                 <div key={note.id} id={`note-${note.id}`} className="motion-preset-expand">
                     <Notes note={note} onDelete={deleteNote} onEdit={handleEdit} />
